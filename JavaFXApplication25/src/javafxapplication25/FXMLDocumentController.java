1e75c3d3ad18565ebf1f13b161dcb36f960b224c
alcatraz <ferdisica@icloud.com>
2021-11-17 20:28:45 +0100

    FristRelease

--- JavaFXApplication25/src/javafxapplication25/FXMLDocumentController.java
+++ JavaFXApplication25/src/javafxapplication25/FXMLDocumentController.java
@@ -24,6 +24,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me if you not i send you an hot image!");
+        System.out.println("You clicked me if you not i send you an hot image!");
         label.setText("Hello World! gruppo twelve");
     }
     

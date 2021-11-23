18daaaa511af3242bb142f56852a2709e920614a
alcatraz <ferdisica@icloud.com>
2021-11-23 20:37:00 +0100

    Resolve Conflict

--- JavaFXApplication25/src/javafxapplication25/FXMLDocumentController.java
+++ JavaFXApplication25/src/javafxapplication25/FXMLDocumentController.java
@@ -23,13 +23,9 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me if you not i send you an hot image!");
-        System.out.println("You clicked me if you not i send you an hot image!");
-        System.out.println("You clicked me if you not i send you an hot image!");
-        System.out.println("ME CLICCAT STRUNZ");
-        label.setText("Hello World! gruppo twelve");
         label.setText("Ciao da Cami");
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
